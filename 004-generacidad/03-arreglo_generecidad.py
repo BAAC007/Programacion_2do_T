650ca1c0a5c9b7cc30fe754e2817bfb5161b8681
Bryan Avila <148177252+BAAC007@users.noreply.github.com>
2025-11-20 10:54:20 +0100

    update

---
+++ 004-generacidad/03-arreglo_generecidad.py
@@ -0,0 +1,14 @@
+numeros = [
+    1,
+    2,
+    "3",
+    4,
+]
+print(numeros)
+
+def calculaDoble():
+    for numero in numeros:
+        numero = int(numero) #convierto en entero
+        print(numero * 2)
+
+calculaDoble()
