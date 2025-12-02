4551f389570513a976ae70075dafc63a6952eb46
Bryan Avila <148177252+BAAC007@users.noreply.github.com>
2025-12-02 12:34:37 +0100

    update

---
+++ 010-Programacion_en_el_lado_del_servidor/07-Operadores.php
@@ -0,0 +1,7 @@
+<?php
+    echo 8+3;
+    echo 3-4;
+    echo 9*5;
+    echo 2/6;
+    echo 1%7;
+?>
---
+++ 010-Programacion_en_el_lado_del_servidor/18-Arrays_multidimensionales.php
@@ -0,0 +1,8 @@
+<?php
+	$agenda = [
+        ["Jose Vicente","Carratala",47],
+        ["Juan","Martinez",45],
+        ["Jaime","lopez",46]
+    ];
+    var_dump($agenda);
+?>
