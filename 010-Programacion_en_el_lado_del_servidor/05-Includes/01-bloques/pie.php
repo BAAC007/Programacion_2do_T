a17eb1bdd13bd2ec62298ed9dd98903e4b895ab5
Bryan Avila <148177252+BAAC007@users.noreply.github.com>
2025-12-07 15:18:21 +0100

    update

--- 010-Programacion_en_el_lado_del_servidor/05-Includes/01-bloques/pie.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer>
-    (c) 2025 Bryan Alejandro Avila Castro
-</footer>
-</body>
-
-</html>
---
+++ 010-Programacion_en_el_lado_del_servidor/08-Sesiones_en_php/02-Incluso_en_diferentes_bloques.php
@@ -0,0 +1,9 @@
+<?php
+	$nombre = "Bryan Alejandro";
+  echo $nombre;
+?>
+
+Esto es HTML pero no pasa nada<br>
+<?php
+	echo $nombre;
+?>
