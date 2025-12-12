f639b212f580ec79b822f471b1c7c7927169ff1e
Bryan Avila <148177252+BAAC007@users.noreply.github.com>
2025-12-12 09:57:39 +0100

    update

--- 010-Programacion_en_el_lado_del_servidor/09-Implantacion_crud/admin/escritorio.php
+++ 010-Programacion_en_el_lado_del_servidor/09-Implantacion_crud/admin/escritorio.php
@@ -2,27 +2,33 @@
 
 <!doctype html>
 <html lang="es">
-	<head>
-  	<title>El Perol - Panel de control</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/estilo.css">
-  </head>
-  <body>
-  	<nav>
-    	<button>Noticias</button>
-      <button>Autores</button>
-    </nav>
-    <main>
-    	<?php
-      	if(isset($_GET['accion'])){								// Si hay "accion" en la URL
-        	if($_GET['accion'] == "nuevo"){					// Si la acción es "nuevo"
-          	include "inc/create/formulario.php";	// En ese caso mete el formulario
-          }
-        }else{																		// En caso contrario
-      		include "inc/read/leer.php"; 						// Enseñame la tabla
-        }
-      ?>
-      <a href="?accion=nuevo" id="nuevo">+</a>
-    </main>
-  </body>
+
+<head>
+  <title>El Perol - Panel de control</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+  <nav>
+    <button>Noticias</button>
+    <button>Autores</button>
+  </nav>
+  <main>
+    <?php
+    // Esto se conoce como router /////////////
+    if (isset($_GET['accion'])) {
+      if ($_GET['accion'] == "nuevo") {
+        include "inc/create/formulario.php";
+      } else if ($_GET['accion'] == "eliminar") { // Defino la acción eliminar
+        include "inc/delete/eliminar.php";    // En ese caso incluyo eliminar.php
+      }
+    } else {
+      include "inc/read/leer.php";
+    }
+    ?>
+    <a href="?accion=nuevo" id="nuevo">+</a>
+  </main>
+</body>
+
 </html>
